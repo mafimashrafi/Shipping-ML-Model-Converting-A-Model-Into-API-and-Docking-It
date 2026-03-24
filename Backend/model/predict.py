0b99ee9a578f44cf60d76ffb55aa27cad2d4e7b5
Mashrafi Rahman <mashrafirahman@Mashrafis-MacBook-Air.local>
2026-03-25 00:11:12 +0600

    Added Confidence score

--- Backend/model/predict.py
+++ Backend/model/predict.py
@@ -9,9 +9,20 @@ model_path = os.path.join(BASE_DIR, 'model.pkl')
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
     
+class_labels = model.classes_.tolist()
+    
 def predict_output(user_input: dict):
     
     input_df = pd.DataFrame(user_input)
-    output = model.predict(input_df)
+    predicted_class = model.predict(input_df)[0]
     
-    return output
+    probabilities = model.predict_proba(input_df)[0]
+    confidence = max(probabilities)
+    
+    class_probs = dict(zip(class_labels, map(lambda p: round(p, 4), probabilities)))
+    
+    return {
+        "Predicted Catgory": predicted_class,
+        "Confidence": confidence,
+        "All Class Probabilites": class_probs
+    }
