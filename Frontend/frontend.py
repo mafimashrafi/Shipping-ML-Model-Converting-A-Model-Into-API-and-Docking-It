9ea82ab960530a11a8904583aa6751874d8988e1
Mashrafi Rahman <mashrafirahman@Mashrafis-MacBook-Air.local>
2026-03-31 15:31:21 +0600

    No changes

--- Frontend/frontend.py
+++ Frontend/frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict" 
+API_URL = "http://backend:8000/predict" 
 
 st.title("Insurance Premium Category Predictor")
 st.markdown("Enter your details below:")
